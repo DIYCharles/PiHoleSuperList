a4d4dd988b6b52a2c8eb8d85b148495935d0aa3d
DIYCharles <mcahneriii@gmail.com>
2022-05-11 23:03:53 -0500

    05/11/2022:23:03:52.76

--- sort.ps1
+++ sort.ps1
@@ -7,7 +7,7 @@ Clear-Content C:\DEV\blacklists\PiHoleSuperList\FINALSU.TXT
 echo ---sorting list and deleating duplicates transfering to FINALSU----
 
 
-Get-Content C:\DEV\blacklists\PiHoleSuperList\FINAL.TXT | Foreach {$_ -replace "\|\|", ""} | Sort-Object | Get-Unique > C:\DEV\blacklists\PiHoleSuperList\FINALSU.TXT
+Get-Content C:\DEV\blacklists\PiHoleSuperList\FINAL.TXT | Foreach {$_ -replace "0\.0\.0\.0", ""}  | Foreach {$_ -replace "\|\|", ""} | Sort-Object | Get-Unique > C:\DEV\blacklists\PiHoleSuperList\FINALSU.TXT
 
 echo ---clearing final file---
 
@@ -15,7 +15,7 @@ Clear-Content C:\DEV\blacklists\PiHoleSuperList\FINAL.TXT
 
 echo ---moving back FINALSU to FINAL---
 
-Get-Content C:\DEV\blacklists\PiHoleSuperList\FINALSU.TXT | Foreach {$_ -replace "0\.0\.0\.0", ""}  | Sort-Object | Get-Unique > C:\DEV\blacklists\PiHoleSuperList\FINAL.TXT
+Get-Content C:\DEV\blacklists\PiHoleSuperList\FINALSU.TXT | Foreach {"0\.0\.0\.0" + $_}  | Sort-Object | Get-Unique > C:\DEV\blacklists\PiHoleSuperList\FINAL.TXT
 
 echo ---clearing FINALSU file---
 
